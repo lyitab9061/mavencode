5e9ecfd491fafbf39e14cd6cecf7aec8b60600a5
lyitab9061 <l00179088@atu.ie>
2023-10-05 14:58:53 +0100

    first commit

---
+++ src/main/java/com/examplemavencode/App.java
@@ -0,0 +1,13 @@
+package com.examplemavencode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
